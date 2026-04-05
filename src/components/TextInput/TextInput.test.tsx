1a4565e7eb7731562973b0e092375c4b9ebf9249
Rob Pataki <idejohet@gmail.com>
2026-04-05 18:03:44 +0100

    Fix broken TextInput test file

--- src/components/TextInput/TextInput.test.tsx
+++ src/components/TextInput/TextInput.test.tsx
@@ -3,7 +3,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { describe, expect, it, vi } from 'vitest';
-import { TextInput } from './TextInput';
+import TextInput from './TextInput';
 
 describe('TextInput', () => {
   const defaultMockProps = {
